a63eeacbffa5f1505a08a0dc9ea8ff641b8ac8aa
mehdi oumouss <moumouss@starclay.fr>
2019-03-24 21:03:32 +0100

    sql queries

---
+++ sql/sql_queries_course2.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `date` timestamp,
+  PRIMARY KEY (`id`)
+)
+
+insert into table 
+values
